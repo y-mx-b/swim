47f85bf79a0c09f48dcaac02a8a6fd74a58191d9
Slope <matthewna1031@gmail.com>
2022-03-20 22:26:53 +0900

    Updated test, fixed bug in CFString to cstring conversion

--- test/test.c
+++ test/test.c
@@ -12,7 +12,19 @@ int main(void) {
     window *w = window_list.windows[i];
     CFStringRef cf_title = get_window_title(w);
     char *title = cstring_from_CFString(cf_title);
-    printf("%d: %s\n", i, title);
+    char *app_name = cstring_from_CFString(get_application_name((get_window_application(w))));
+    CGRect frame = get_window_frame(w);
+
+    printf("Window Number: %d\n", i);
+    printf("\tApplication Name: %s\n", app_name);
+    printf("\tWindow Title: %s\n", title);
+    printf("\tWindow ID: %d\n", get_window_id(w));
+    printf("\tWindow Position: (%.2f, %.2f)\n", frame.origin.x, frame.origin.y);
+    printf("\tWindow Size:\n");
+    printf("\t\tHeight: %.2f\n", frame.size.height);
+    printf("\t\tWidth: %.2f\n", frame.size.width);
+
+    free(app_name);
     free(title);
     destroy_window(w);
   }
